fcb4d23897b3fcdf9b3221ed8b54f79d1698ede6
이상원 <airless86@hanmail.net>
2017-11-05 20:49:19 +0900

    모바일 index페이지 추가

--- src/main/java/kr/or/phonejob/IndexController/IndexController.java
+++ src/main/java/kr/or/phonejob/IndexController/IndexController.java
@@ -208,6 +208,123 @@ public class IndexController {
 		mv.addAttribute("url", url);
 		return rePage;
 	}
-	
+
+
+
+	@RequestMapping(value="s_Main.do", method=RequestMethod.GET)
+	public String moveMobileIndex(Model mv, HttpServletRequest request){
+		logger.info(">>>>>>모바일 Index Page 접근");
+
+		String msg = "";
+
+		//현 접속자의 IP를 구합니다.
+
+		String cIp = request.getHeader("X-Forwarded-For");
+
+		if(cIp == null || cIp.length() == 0 || "unknown".equalsIgnoreCase(cIp)) {
+			cIp = request.getHeader("Proxy-Client-IP");
+		}
+		if(cIp == null || cIp.length() == 0 || "unknown".equalsIgnoreCase(cIp)) {
+			cIp = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if(cIp == null || cIp.length() == 0 || "unknown".equalsIgnoreCase(cIp)) {
+			cIp = request.getHeader("HTTP_CLIENT_IP");
+		}
+		if(cIp == null || cIp.length() == 0 || "unknown".equalsIgnoreCase(cIp)) {
+			cIp = request.getHeader("HTTP_X_FORWARDED_FOR");
+		}
+		if(cIp == null || cIp.length() == 0 || "unknown".equalsIgnoreCase(cIp)) {
+			cIp = request.getRemoteAddr();
+		}
+
+		    /*
+	    	허가된 IP목록 입니다.
+	    	아래 배열에 추가 하면 해당 IP에 대해 access가 허용 됩니다.
+		     */
+
+		List<UserIpDto> userip = new ArrayList<UserIpDto>();
+		userip=iservice.getUserIp();
+
+		int iOk = 0;
+		logger.info("접속한 아이피 ! : " + cIp);
+
+		for(int i = 0; i < userip.size(); i++) {
+			//logger.info("허용된 아이피!!" + userip.get(i).getUser_ip().toString());
+			if( userip.get(i).getUser_ip().equals(cIp) ){
+				iOk = 1;
+				break;
+			}
+		}
+
+
+		if( iOk == 0){
+			//response.sendRedirect("deny.jsp");
+			msg="<B>Access Denied : 현재 접속하신 아이피는 " + cIp + "입니다 </B>";
+			mv.addAttribute("msg", msg);
+			mv.addAttribute("cIp", cIp);
+			return "lock";
+		}
+
+
+		//해당 서비스 및 함수는 자동로그인 기능 구현 중입니다.
+		//lservice.checkUserWithSessionKey();
+
+
+		//구인 데이터를 뽑아 오기 위한 작업 시작
+		List<RegisterGooinDto> gooinluxury = new ArrayList<RegisterGooinDto>();
+		List<RegisterGooinDto> gooinbest = new ArrayList<RegisterGooinDto>();
+		List<RegisterGooinDto> gooinnormal = new ArrayList<RegisterGooinDto>();
+
+		try{
+			gooinluxury=gooservice.gooinluxury();
+			gooinbest=gooservice.gooinbest();
+			gooinnormal=gooservice.gooinnormal();
+			/*
+			logger.info("럭셔리광고사이즈 : " + gooinluxury.size());
+
+			for(int r=0; r<gooinluxury.size(); r++){
+				logger.info("럭셔리광고 : " + gooinluxury.get(r).toString());
+			}
+
+			for(int j=0; j<gooinbest.size(); j++){
+				logger.info("베스트광고 : " + gooinbest.get(j).toString());
+			}
+
+			for(int k=0; k<gooinnormal.size(); k++){
+				logger.info("노말광고 : " + gooinnormal.get(k).toString());
+			}
+			*/
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+
+		//인재 정보를 불러 오기 위한 Dto 객체 생성
+		List<RegisterGoogicDto> result = new ArrayList<RegisterGoogicDto>();
+		try{
+			result =  gservice.selectGoogic();
+
+			for(int l=0; l<result.size(); l++){
+				//logger.info("인재정보 masking 전 : " + result.get(l).toString());
+				result.get(l).setUsername(MaskingUtil.getMaskingName(result.get(l).getUsername()));
+				result.get(l).setUserid(MaskingUtil.getMaskingId(result.get(l).getUserid()));
+				//logger.info("인재정보 masking 후 : " + result.get(l).toString());
+			}
+		}catch(Exception e){
+			logger.error(e.getMessage());
+		}
+
+		mv.addAttribute("result", result);
+		mv.addAttribute("gooinluxury", gooinluxury);
+		mv.addAttribute("gooinbest", gooinbest);
+		mv.addAttribute("gooinnormal", gooinnormal);
+
+		return "home.index";
+	}
+
+
+
+
 
 }
