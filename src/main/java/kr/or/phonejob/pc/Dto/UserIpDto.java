8948c4c3a3a4c232c72d5a73e539ba4d9553a58d
이상원 <airless86@hanmail.net>
2018-03-27 23:19:00 +0900

    1. ip 등록 시 등록자 정보 테이블에 저장 처리

--- src/main/java/kr/or/phonejob/pc/Dto/UserIpDto.java
+++ src/main/java/kr/or/phonejob/pc/Dto/UserIpDto.java
@@ -3,43 +3,49 @@ package kr.or.phonejob.pc.Dto;
 public class UserIpDto {
 
 	private String ip_name;
-	private String user_name;
+	private String register;
 	private String user_ip;
 	private String user_phone;
-	
-	
-	
-	
+
 	public String getIp_name() {
 		return ip_name;
 	}
+
 	public void setIp_name(String ip_name) {
 		this.ip_name = ip_name;
 	}
-	public String getUser_phone() {
-		return user_phone;
+
+	public String getRegister() {
+		return register;
 	}
-	public void setUser_phone(String user_phone) {
-		this.user_phone = user_phone;
+
+	public void setRegister(String register) {
+		this.register = register;
 	}
-	public String getUser_name() {
-		return user_name;
-	}
+
 	public String getUser_ip() {
 		return user_ip;
 	}
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
-	}
+
 	public void setUser_ip(String user_ip) {
 		this.user_ip = user_ip;
 	}
-	@Override
-	public String toString() {
-		return "UserIpDto [ip_name=" + ip_name + ", user_name=" + user_name + ", user_ip=" + user_ip + ", user_phone="
-				+ user_phone + "]";
+
+	public String getUser_phone() {
+		return user_phone;
 	}
 
-	
-	
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
+	}
+
+	@Override
+	public String toString() {
+		return "UserIpDto{" +
+				"ip_name='" + ip_name + '\'' +
+				", register='" + register + '\'' +
+				", user_ip='" + user_ip + '\'' +
+				", user_phone='" + user_phone + '\'' +
+				'}';
+	}
 }
